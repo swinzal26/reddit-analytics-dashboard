61652ed28f60fcac5c2e5620427d441d6a6b7cc3
Swinzal <ss4459@cornell.edu>
2026-06-30 00:20:00 +0530

    Configure API for production deployment

--- frontend/src/api/posts.js
+++ frontend/src/api/posts.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.DEV
+    ? '/api'
+    : import.meta.env.VITE_API_URL,
 })
 
 export const fetchPosts = () => api.get('/posts/')
