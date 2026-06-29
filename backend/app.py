1eb4945cf60063f76b425b0e8717190498a9b5df
Swinzal <ss4459@cornell.edu>
2026-06-30 00:21:33 +0530

    Configure API for production deployment

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,10 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 # Keys: integer post IDs
 # Values: post dict objects matching API spec
